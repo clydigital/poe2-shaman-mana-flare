fbe7cb6a72c4ad9ae5fbff13a772b04426c0812f
clydigital <leeyanghere@gmail.com>
2026-08-19 00:17:05 +0800

    Load sortable table controls in guide build

--- scripts/build_v47.py
+++ scripts/build_v47.py
@@ -9,6 +9,7 @@ needle = '</body>'
 scripts = [
     '<script src="./v47.js"></script>\n',
     '<script src="./v48-topology.js"></script>\n',
+    '<script src="./v54-sortable-tables.js"></script>\n',
 ]
 if needle not in s:
     raise SystemExit('body closing tag missing')
@@ -16,4 +17,4 @@ for script in scripts:
     if script not in s:
         s = s.replace(needle, script + needle, 1)
 p.write_text(s, encoding='utf-8')
-print('built v47 + topology v2', len(s))
+print('built v47 + topology v2 + sortable tables', len(s))
